6dce2f7dec57f386cf2e2399b2b0e5ddb229d737
fsm12 <fsm1204@naver.com>
2024-09-07 15:11:42 +0900

    setting: 백엔드 spring 환경 설정

---
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/StarcastApplication.java
@@ -0,0 +1,13 @@
+package com.mobyeoldol.starcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarcastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarcastApplication.class, args);
+	}
+
+}
---
+++ backend/starcast/src/test/java/com/mobyeoldol/starcast/StarcastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobyeoldol.starcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarcastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
