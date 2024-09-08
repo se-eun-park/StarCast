02e21e54493e7acc40b1473c6f3a7ced2b53b911
fsm12 <fsm1204@naver.com>
2024-09-08 16:42:47 +0900

    setting: 백엔드 spring 환경 설정

---
+++ backend/data-collector/src/test/java/com/mobyeoldol/data_collector/DataCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobyeoldol.data_collector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
