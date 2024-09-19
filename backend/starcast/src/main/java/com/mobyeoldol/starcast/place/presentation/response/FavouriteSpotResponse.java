e83087552cd0133b519775e573e4538a5f0d9508
박소미 <fsm1204@naver.com>
2024-09-19 18:25:32 +0900

    #7 refactor: 사용하지 않는 import 제거

--- backend/starcast/src/main/java/com/mobyeoldol/starcast/place/presentation/response/FavouriteSpotResponse.java
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/place/presentation/response/FavouriteSpotResponse.java
@@ -2,12 +2,11 @@ package com.mobyeoldol.starcast.place.presentation.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class FavouriteSpotResponse {
-    private String spot_uid;  // 즐겨찾기 아이디
+    private String spot_uid;
 }
