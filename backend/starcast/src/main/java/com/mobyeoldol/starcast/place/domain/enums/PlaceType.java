b8fe6b565d0cb7b4c88e2f1274c5d76ddeffeaa5
박소미 <fsm1204@naver.com>
2024-09-20 10:10:09 +0900

    #7 refactor: Enum에 한국어 이름 필드 추가

--- backend/starcast/src/main/java/com/mobyeoldol/starcast/place/domain/enums/PlaceType.java
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/place/domain/enums/PlaceType.java
@@ -1,6 +1,13 @@
 package com.mobyeoldol.starcast.place.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
 public enum PlaceType {
-    EUP_MYUN_DONG,
-    OBSERVATORY
+    EUP_MYUN_DONG("읍면동"),
+    OBSERVATORY("천문대");
+
+    private final String koreanName;
 }
