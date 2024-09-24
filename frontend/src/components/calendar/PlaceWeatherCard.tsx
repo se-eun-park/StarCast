baf1b174728dccc3324548525a2e7abaec70f0c5
박세은 <seeunbag30@gmail.com>
2024-09-24 13:50:40 +0900

    #6 refactor: placeWeatherCard 타입 파일 분리

--- frontend/src/components/calendar/PlaceWeatherCard.tsx
+++ frontend/src/components/calendar/PlaceWeatherCard.tsx
@@ -1,39 +1,7 @@
 import { useState } from 'react'
+import { GetPlaceWeatherResponse } from '../../types/apis'
 import AstronomyInfoWidget from '@components/common/AstronomyInfoWidget'
 import { ArrowIcon, LocationIcon, PlusIcon } from '@assets/svg/calendar'
-
-type WeatherDetail = {
-  castarPoint: number
-  cloudCoverage: number
-  precipitation: number
-}
-
-type WeatherOfTheNight = {
-  [key: `hour${string}`]: WeatherDetail
-}
-
-type Best = {
-  bestCastarPoint: number
-  hour: string
-  cloudCoverage: number
-  lightPollution: number
-  moonPhase: number
-  humidity: number
-}
-
-type IsPlanned = {
-  hour: string
-  spot_uid: string
-}
-
-type placeWeatherCardProps = {
-  place_uid: string
-  details: string
-  weatherOfTheNight: WeatherOfTheNight
-  best: Best
-  moonSetTime: string
-  isPlanned: IsPlanned | null
-}
 
 const PlaceWeatherCard = ({
   place_uid,
@@ -42,7 +10,7 @@ const PlaceWeatherCard = ({
   best,
   moonSetTime,
   isPlanned,
-}: placeWeatherCardProps) => {
+}: GetPlaceWeatherResponse) => {
   const scoreStatus =
     best.bestCastarPoint >= 75 ? '좋음' : best.bestCastarPoint >= 40 ? '보통' : '나쁨'
 
