0096070a7b792083a0ca4128780c3a85779362da
박소미 <fsm1204@naver.com>
2024-09-25 14:39:12 +0900

    #21 refactor: 사용하지 않은 import 삭제

--- backend/starcast/src/main/java/com/mobyeoldol/starcast/place/domain/FavouriteSpot.java
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/place/domain/FavouriteSpot.java
@@ -8,10 +8,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
