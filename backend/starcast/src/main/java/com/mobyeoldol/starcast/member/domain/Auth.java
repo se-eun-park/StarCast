a4b1eae077471f03a82966437129d7d42343502d
박소미 <fsm1204@naver.com>
2024-09-25 15:32:16 +0900

    #21 refactor: 사용하지 않은 import 삭제

--- backend/starcast/src/main/java/com/mobyeoldol/starcast/member/domain/Auth.java
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/member/domain/Auth.java
@@ -6,10 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
