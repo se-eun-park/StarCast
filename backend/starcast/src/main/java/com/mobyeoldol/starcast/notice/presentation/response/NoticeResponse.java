85727d114080ae283df4ac18f54e0df53cd653bc
박소미 <fsm1204@naver.com>
2024-09-26 17:28:50 +0900

    #10 feat: 알림 수신여부 변경 API Controller에 선언

---
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/notice/presentation/response/NoticeResponse.java
@@ -0,0 +1,14 @@
+package com.mobyeoldol.starcast.notice.presentation.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class NoticeResponse {
+    boolean settingType;
+}
