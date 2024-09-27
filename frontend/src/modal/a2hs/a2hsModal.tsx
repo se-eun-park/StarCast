109bb3a9b6c127af0e9634377feb6d32447e4450
하림 <ez703@naver.com>
2024-09-27 16:51:29 +0900

    #20 design: a2hs 모달 패딩 및 폰트 컬러 수정

--- frontend/src/modal/a2hs/a2hsModal.tsx
+++ frontend/src/modal/a2hs/a2hsModal.tsx
@@ -44,14 +44,18 @@ function A2HSModal() {
     <div
       className={`${a2hs ? '' : 'hidden'} z-10 w-full h-full fixed left-0 bottom-0 flex items-end justify-center bg-black bg-opacity-70`}
     >
-      <div className='w-[37.375rem] px-4 py-6 flex flex-col justify-center items-center rounded-t-3xl bg-bg-800 bottom-0 z-30 '>
+      <div className='w-[37.375rem] pt-8 pb-4 py-6 flex flex-col justify-center items-center rounded-t-3xl bg-bg-800 bottom-0 z-30 '>
         <div className='w-full flex flex-col justify-center items-center gap-8'>
           <div className='w-[17.75rem] h-[13.3125rem] flex flex-col items-center bottom-8 gap-4'>
             <SvgCastar3DIcon className='w-[6.25rem] h-[7.1875rem]' />
             <div>
-              <p className='text-2xl text-center text-primary-light'>똑똑, 스타캐스트에요.</p>
-              <p className='text-2xl text-center text-white'>홈 화면에 들어가도 될까요?</p>
-              <p className='text-xs text-center py-1 text-white'>
+              <p className='text-2xl text-center font-medium text-primary-light'>
+                똑똑, 스타캐스트에요.
+              </p>
+              <p className='text-2xl text-center font-medium text-white'>
+                홈 화면에 들어가도 될까요?
+              </p>
+              <p className='text-xs text-center py-1 text-text-secondary'>
                 지금 홈 화면에 추가하지 않고 나중에 할 수도 있어요.
               </p>
             </div>
@@ -61,7 +65,7 @@ function A2HSModal() {
             <button
               onClick={installApp}
               type='button'
-              className='text-sm px-[8.0625rem] py-[.625rem] flex items-center justify-center bg-primary rounded-full font-bold text-bg-900'
+              className='text-sm px-[8.0625rem] py-[.625rem] flex items-center justify-center bg-btn-primarybg rounded-full font-bold text-btn-text'
             >
               당연하죠!
             </button>
@@ -69,7 +73,7 @@ function A2HSModal() {
             <button
               onClick={() => setA2hs(undefined)}
               type='button'
-              className='text-xs px-5 py-2 flex items-center justify-center rounded-full text-bg-100 font-bold'
+              className='text-xs px-5 py-2 flex items-center justify-center rounded-full text-btn-secondarytext font-bold'
             >
               조금 더 친해진 뒤에 할게요
             </button>
