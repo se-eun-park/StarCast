d6081303151f49bd078d4d3062e4ce68da6f39f7
SoMi Park <fsm1204@naver.com>
2024-09-28 20:16:13 +0900

    #28 fix: 엔티티 연관관계가 양방향이 되도록 수정

--- backend/starcast/src/main/java/com/mobyeoldol/starcast/place/domain/repository/FavouriteSpotRepository.java
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/place/domain/repository/FavouriteSpotRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface FavouriteSpotRepository extends JpaRepository<FavouriteSpot, String> {
-    Optional<FavouriteSpot> findByPlaceUidAndProfileUid(String placeUid, String profileUid);
+    Optional<FavouriteSpot> findByPlace_PlaceUidAndProfile_ProfileUid(String placeUid, String profileUid);
 }
