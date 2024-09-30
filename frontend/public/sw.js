7fbe0883fc10635ac99b24974c6f07afccb06dfc
하림 <ez703@naver.com>
2024-09-30 15:02:20 +0900

    #34 chore: sw.js 캐싱 관련 코드 수정

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'StarcastCache'
+const CACHE_NAME = 'StarcastCache-v1' // 버전 관리
 const urlsToCache = ['/', '/index.html', '/favicon.ico', '/manifest.json', '/static/js/bundle.js']
 
 self.addEventListener('install', (event) => {
@@ -28,20 +28,28 @@ self.addEventListener('activate', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      // 캐시가 있으면 캐시 리소스를 반환
-      if (response) {
-        return response
+    caches.match(event.request).then((cachedResponse) => {
+      // 캐시가 존재하면 캐시된 리소스를 반환
+      if (cachedResponse) {
+        // 항상 네트워크 요청을 하여 업데이트
+        fetch(event.request).then((networkResponse) => {
+          if (networkResponse && networkResponse.status === 200) {
+            const responseToCache = networkResponse.clone()
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, responseToCache)
+            })
+          }
+        })
+        return cachedResponse
       }
 
-      // 네트워크 요청을 처리하고 캐시에 추가
+      // 캐시가 없으면 네트워크 요청
       return fetch(event.request).then((networkResponse) => {
         if (!networkResponse || networkResponse.status !== 200) {
           return networkResponse
         }
 
         const responseToCache = networkResponse.clone()
-
         caches.open(CACHE_NAME).then((cache) => {
           cache.put(event.request, responseToCache)
         })
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,7 +1,9 @@
 const LoginPage = () => {
   return (
-    <div>
-      <h1>로그인 페이지</h1>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <p className='text-2xl text-center'>
+        가장 쉬운 별빛 여행 <br /> 스타캐스트
+      </p>
     </div>
   )
 }
