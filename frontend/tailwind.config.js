3f435af75b6e9afcadbcc473eef6277d55dabc74
Wonwoo Cho <ww8637@naver.com>
2024-09-30 13:13:57 +0900

    #27 design: text-xl, lg, md 어색한 행간 조정

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,9 +9,9 @@ export default {
     fontSize: {
       '3xl': ['1.5rem', { lineHeight: '2rem' }], 
       '2xl': ['1.375rem', { lineHeight: '1.875rem' }], 
-      'xl': ['1.25rem', { lineHeight: '1.75rem' }], 
-      'lg': ['1.125rem', { lineHeight: '1.625rem' }],  
-      'md': ['1rem', { lineHeight: '1.5rem' }],      
+      'xl': ['1.25rem', { lineHeight: '1.625rem' }], 
+      'lg': ['1.125rem', { lineHeight: '1.5rem' }],  
+      'md': ['1rem', { lineHeight: '1.375rem' }],      
       'sm': ['0.875rem', { lineHeight: '1.25rem' }], 
       'xs': ['0.75rem', { lineHeight: '1rem' }],     
       '2xs': ['0.6875rem', { lineHeight: '0.875rem' }],
