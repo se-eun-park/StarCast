a5ca36d0db590d437b8e1a9513805bc8e4eec125
SoMi Park <fsm1204@naver.com>
2024-10-01 14:12:37 +0900

    #35 fix: 사용하지 않은 import 삭제

--- backend/starcast/src/main/java/com/mobyeoldol/starcast/test/TestController.java
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/test/TestController.java
@@ -3,7 +3,6 @@ package com.mobyeoldol.starcast.test;
 import com.mobyeoldol.starcast.global.template.BaseResponseTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
