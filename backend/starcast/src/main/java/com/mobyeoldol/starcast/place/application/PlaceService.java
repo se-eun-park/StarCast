75f5e6200718c510d8fddd8d95ff2f0aee51349f
박소미 <fsm1204@naver.com>
2024-10-02 14:07:24 +0900

    #14 feat: 관측지 리스트 조회 API에 검색어 유사도 알고리즘 및 정렬 기준 처리 추가

--- backend/starcast/src/main/java/com/mobyeoldol/starcast/place/application/PlaceService.java
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/place/application/PlaceService.java
@@ -3,15 +3,19 @@ package com.mobyeoldol.starcast.place.application;
 import com.mobyeoldol.starcast.place.domain.FavouriteSpot;
 import com.mobyeoldol.starcast.place.domain.enums.MainPlace;
 import com.mobyeoldol.starcast.place.presentation.request.CreatePlanRequest;
+import com.mobyeoldol.starcast.place.presentation.request.GetPlaceListRequest;
 import com.mobyeoldol.starcast.place.presentation.request.ModifyPlanRequest;
+import com.mobyeoldol.starcast.place.presentation.response.GetPlaceListResponse;
 import com.mobyeoldol.starcast.place.presentation.response.PlaceDetailsResponse;
 import com.mobyeoldol.starcast.place.presentation.response.PlanDetailsResponse;
 import com.mobyeoldol.starcast.place.presentation.response.PlanUidResponse;
+import jakarta.validation.Valid;
 
 public interface PlaceService {
     public FavouriteSpot createFavourite(String placeUid, String profileUid);
     public void deleteFavourite(String spotUid);
     public PlaceDetailsResponse getPlaceDetails(String placeUid);
+    public GetPlaceListResponse getPlaceList(GetPlaceListRequest request);
 
     public PlanUidResponse makePlan(CreatePlanRequest request, String profileUid);
     public PlanDetailsResponse getPlanDetails(String planUid, String profileUid);
@@ -19,4 +23,5 @@ public interface PlaceService {
     public void deletePlan(String planUid, String profileUid);
 
     void updateActionPlaceType(String profileUid, MainPlace mainPlace);
+
 }
--- backend/starcast/src/main/java/com/mobyeoldol/starcast/place/domain/repository/PlaceRepository.java
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/place/domain/repository/PlaceRepository.java
@@ -1,9 +1,13 @@
 package com.mobyeoldol.starcast.place.domain.repository;
 
 import com.mobyeoldol.starcast.place.domain.Place;
+import com.mobyeoldol.starcast.place.domain.enums.PlaceType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,4 +15,8 @@ public interface PlaceRepository extends JpaRepository<Place, String> {
     Optional<Place> findByPlaceUid(String placeUid);
 
     Optional<Place> findByAddress1AndAddress2AndAddress3AndAddress4(String address1, String address2, String address3, String address4);
+    List<Place> findByType(PlaceType type);
+
+    @Query("SELECT COUNT(c) FROM Community c WHERE c.place = :place")
+    int countCommunitiesByPlace(@Param("place") Place place);
 }
