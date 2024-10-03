979d99ce7d44503cfef0937f547c160b4b1c6381
하림 <ez703@naver.com>
2024-10-04 08:57:05 +0900

    #34 design: 버튼 ui 수정

--- frontend/src/components/login/LoginButton.tsx
+++ frontend/src/components/login/LoginButton.tsx
@@ -12,7 +12,7 @@ const LoginButton: React.FC = () => {
     <div className='fixed w-[22.5rem] bottom-0 flex justify-center items-center'>
       <button
         onClick={handleClick}
-        className='bg-btn-primary-bg text-btn-primary-text text-sm my-6 text-center font-paperlogy font-semibold py-[.625rem] px-[3.125rem] rounded-full'
+        className='bg-btn-primary-bg text-btn-primary-text w-full text-sm my-6 mw-4 text-center font-paperlogy font-semibold py-[.625rem] px-[3.125rem] rounded-full'
       >
         카카오 계정으로 시작하기
       </button>
