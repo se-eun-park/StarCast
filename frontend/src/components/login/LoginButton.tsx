501bc9701f491d48642754bec66c065154b378f0
하림 <ez703@naver.com>
2024-10-03 19:03:32 +0900

    #34 design: loginbutton component 생성 및 ui 디자인

---
+++ frontend/src/components/login/LoginButton.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+const LoginButton: React.FC = () => {
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/home')
+  }
+
+  return (
+    <div className='fixed w-[22.5rem] bottom-0 flex justify-center items-center'>
+      <button
+        onClick={handleClick}
+        className='bg-btn-primary-bg text-btn-primary-text text-sm my-6 text-center font-paperlogy font-semibold py-[.625rem] px-[3.125rem] rounded-full'
+      >
+        카카오 계정으로 시작하기
+      </button>
+    </div>
+  )
+}
+
+export default LoginButton
--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,9 +1,12 @@
+import LoginButton from '@components/login/LoginButton'
+
 const LoginPage = () => {
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <p className='text-2xl text-center'>
         가장 쉬운 별빛 여행 <br /> 스타캐스트
       </p>
+      <LoginButton />
     </div>
   )
 }
