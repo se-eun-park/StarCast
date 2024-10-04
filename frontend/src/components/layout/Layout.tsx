fa766580f2c1c5ce3f3d647702413519ba877231
박세은 <seeunbag30@gmail.com>
2024-10-04 18:04:49 +0900

    #41 docs: 레이아웃 보더 삭제

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: PropsWithChildren) {
   return (
     <div className='w-full h-full bg-bgGradient'>
       <div className='w-full min-h-dvh'>
-        <div className='sm:w-[37.5rem] w-full mx-auto min-h-dvh sm:border-l sm:border-r sm:border-white'>
+        <div className='sm:w-[37.5rem] w-full mx-auto min-h-dvh'>
           <NavbarWithLabel />
           <Outlet />
           {children}
