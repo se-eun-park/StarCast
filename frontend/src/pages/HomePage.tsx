484b47901ca6b715a7a6b491bd18ed0e86e12959
박세은 <seeunbag30@gmail.com>
2024-10-04 11:57:45 +0900

    #41 docs: layout 요소들 바뀐 폴더 경로 반영

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,10 +1,14 @@
 import A2HSModal from '@modal/a2hs/a2hsModal'
 import SkyBackground from '@components/home/SkyBackground'
+import HomeContentsLayout from '@components/layout/HomeContentsLayout'
 
 const HomePage = () => {
   return (
     <div className='relative w-full h-screen'>
       <SkyBackground />
+      <HomeContentsLayout>
+        <div>하이? ㅌㅋㅋㅋㅌㅋ</div>
+      </HomeContentsLayout>
       <A2HSModal />
     </div>
   )
