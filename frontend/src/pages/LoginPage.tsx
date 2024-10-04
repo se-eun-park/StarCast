1843dedd1760916a1461bc4660d68f2a7f7ed7ab
하림 <ez703@naver.com>
2024-10-04 14:52:25 +0900

    #34 feat: 로그인 진입 가이드 캐러셀

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,13 +1,19 @@
 import LoginButton from '@components/login/LoginButton'
+import LoginCarousel from '@components/login/Carousel'
+import '@/components/login/CarouselCSS.css'
 
 const LoginPage = () => {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <p className='text-2xl text-center'>
-        가장 쉬운 별빛 여행 <br /> 스타캐스트
-      </p>
-      <LoginButton />
-    </div>
+    <>
+      <div className='flex flex-col items-center justify-center'>
+        <LoginCarousel
+          SLIDES={['/carousel/carousel1.png', '/carousel/carousel2.png', '/carousel/carousel3.png']}
+          OPTIONS={{ loop: false }}
+        />
+        {/* 로그인 버튼을 같은 div 안에 두어 도트 버튼 바로 아래에 위치하도록 설정 */}
+        <LoginButton />
+      </div>
+    </>
   )
 }
 
