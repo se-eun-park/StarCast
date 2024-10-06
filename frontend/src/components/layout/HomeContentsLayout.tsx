c80d1f24661d581c0f9ebff76b1c9cd616990fe1
박세은 <seeunbag30@gmail.com>
2024-10-06 17:25:27 +0900

    #41 fix: homeContentsLayout 최소 높이 설정

--- frontend/src/components/layout/HomeContentsLayout.tsx
+++ frontend/src/components/layout/HomeContentsLayout.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react'
 
 const HomeContentsLayout = ({ children }: PropsWithChildren) => {
   return (
-    <div className='relative flex flex-col items-center w-full h-full bg-bg-900'>
+    <div className='relative flex flex-col items-center w-full h-full min-h-[calc(100vh-20rem)] bg-bg-900'>
       <div className='z-40'>{children}</div>
       <div className='absolute'>
         <div className='relative flex min-w-[27.9375rem] top-[-3.625rem] h-fit'>
