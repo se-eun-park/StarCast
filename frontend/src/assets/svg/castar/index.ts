8025998eb0ba77b9f5070b14808318c1139543e1
박세은 <seeunbag30@gmail.com>
2024-10-06 15:25:30 +0900

    #41 asset: 캐스타 점수 설명 모달에 사용할 svg 추가

--- frontend/src/assets/svg/castar/index.ts
+++ frontend/src/assets/svg/castar/index.ts
@@ -1,3 +1,4 @@
 export { default as HappyCastarIcon } from './HappyCastarIcon'
 export { default as NormalCastarIcon } from './NormalCastarIcon'
 export { default as SadCastarIcon } from './SadCastarIcon'
+export { default as SmartCastarIcon } from './SmartCastarIcon'
