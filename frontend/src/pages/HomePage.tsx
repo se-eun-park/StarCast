c1f5cb98cf5b24367d0bf01074760540e5bfc3d7
박세은 <seeunbag30@gmail.com>
2024-10-06 18:25:47 +0900

    #41 feat: homePage UI 구현

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,6 +1,10 @@
 import A2HSModal from '@modal/a2hs/a2hsModal'
 import SkyBackground from '@components/home/SkyBackground'
 import HomeContentsLayout from '@components/layout/HomeContentsLayout'
+import CastaScoreGauge from '@components/home/CastaScoreGauge'
+import CastaScoreReaction from '@components/home/CastaScoreReaction'
+import AstronomyInfoWidget from '@components/common/AstronomyInfoWidget'
+import PlannedWidget from '@components/home/PlannedWidget'
 
 const HomePage = () => {
   return (
@@ -8,7 +12,27 @@ const HomePage = () => {
       <SkyBackground />
       <div className='mt-80'>
         <HomeContentsLayout>
-          <div className='w-20 h-[600px] bg-btn-tertiary-text'>하이? ㅌㅋㅋㅋㅌㅋ</div>
+          <div className='relative flex items-end justify-center mr-5 space-x-2 -top-4'>
+            <CastaScoreGauge />
+            <CastaScoreReaction status='좋음' />
+          </div>
+          <div className='w-full h-full px-12 py-4 bg-gradient900to800 rounded-3xl'>
+            <AstronomyInfoWidget
+              buttonBgColor='bg-bg-50/10'
+              bgColor='bg-bg-50/5'
+              textAlign='text-center'
+              details='역삼동'
+              hour='22'
+              cloudCoverage={1}
+              lightPollution={1}
+              moonPhase={2}
+              precipitation={0}
+              moonSetTime='00:00'
+            />
+          </div>
+          <div className='flex justify-center mt-3'>
+            <PlannedWidget />
+          </div>
         </HomeContentsLayout>
       </div>
       <A2HSModal />
