25373dad7f376968342eb07f25eb995bfb82db2c
kwakseongjae <gkffhdnls13@gmail.com>
2024-10-06 21:58:49 +0900

    fix: topbar controller z 인덱스 수정

--- frontend/src/components/home/topbar-controller/index.tsx
+++ frontend/src/components/home/topbar-controller/index.tsx
@@ -3,7 +3,7 @@ import UserController from '@components/home/topbar-controller/UserController.ts
 
 export default function TopbarController() {
   return (
-    <div className='fixed top-0 flex justify-between p-4 w-[37.5rem] strong-blur'>
+    <div className='fixed z-10 top-0 flex justify-between p-4 w-[37.5rem] strong-blur'>
       <LocationController />
       <UserController />
     </div>
