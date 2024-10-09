53506b69ffc25c0848ea0462dd2760d8f1f4a41b
Dada-none <gadin8631@naver.com>
2024-10-09 17:41:17 +0900

    #53 fix : json 역직렬화 오류 수정

--- backend/starcast/src/main/java/com/mobyeoldol/starcast/auth/presentation/response/UnlinkResponse.java
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/auth/presentation/response/UnlinkResponse.java
@@ -1,12 +1,18 @@
 package com.mobyeoldol.starcast.auth.presentation.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 
 @Getter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor // 기본 생성자 추가
 public class UnlinkResponse {
+
+    @JsonProperty("id") // 필요 시 명시적으로 JSON 속성과 매핑
     private long id;
 }
