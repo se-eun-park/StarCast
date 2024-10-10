44c5713633a89a77ed208a848c144ab7fbe2ffc2
강다영 <gadin8631@naver.com>
2024-10-10 16:47:22 +0900

    fix: 프론트 요구 사항 반영

--- backend/starcast/src/main/java/com/mobyeoldol/starcast/auth/presentation/AuthController.java
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/auth/presentation/AuthController.java
@@ -14,10 +14,8 @@ import com.mobyeoldol.starcast.auth.presentation.response.UpdateUserInfoTmpRespo
 import com.mobyeoldol.starcast.global.template.BaseResponseTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.view.RedirectView;
 
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.Cookie;
@@ -31,28 +29,22 @@ import java.util.Optional;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
 
-    @Value("${kakao.redirect.uri.signup}")
-    String kakaoRedirectUriSignup;
-
-    @Value("${kakao.redirect.uri.home}")
-    String kakaoRedirectUriHome;
-
     private final AuthService authService;
     private final AuthRepository authRepository;
     private final RefreshTokenRepository refreshTokenRepository;
 
     @GetMapping("/login")
-    public RedirectView getAccessCode() {
+    public ResponseEntity<BaseResponseTemplate<?>> getAccessCode() {
         log.info("[login 유효 인가 코드 요청 API] GET /api/v1/auth/login");
-        RedirectView response = new RedirectView();
-        response.setUrl(authService.getAccessCode());
+        String url = authService.getAccessCode();
 
-        log.info("[login 유효 인가 코드 요청 API] 인가 코드 받아서 리턴");
-        return response;
+        log.info("[login 유효 인가 코드 요청 API] 인가 코드 받아서 프론트로 리턴");
+        BaseResponseTemplate<String> successResponse = BaseResponseTemplate.success(url);
+        return ResponseEntity.ok().body(successResponse);
     }
 
     @GetMapping("/redirect-login")
-    public RedirectView getAccessToken(@RequestParam("code") String code, HttpServletResponse httpServletResponse) {
+    public ResponseEntity<BaseResponseTemplate<?>> getAccessToken(@RequestParam("code") String code, HttpServletResponse httpServletResponse) {
         log.info("[login 카카오에 토큰 요청 API] GET /api/v1/auth/redirect-login");
         KakaoTokenResponseDto responseDto = authService.getAccessToken(code);
 
@@ -83,21 +75,21 @@ public class AuthController {
         log.info("[login 카카오에 토큰 요청 API] kakao id로 auth 엔티티 찾기");
         Optional<Auth> optionalAuth = authRepository.findByKakaoUid(String.valueOf(userInfo.getId()));
 
-        RedirectView redirectView = new RedirectView();
+        BaseResponseTemplate<Boolean> successResponse;
 
         if(optionalAuth.isEmpty()) {
             log.info("[login 카카오에 토큰 요청 API] auth 엔티티 존재하지 않음. 따라서 auth 엔티티, userInfoTmp 엔티티 생성");
             authService.generateAuthAndUserInfoTmp(userInfo);
 
             log.info("[login 카카오에 토큰 요청 API] 클라이언트를 동의 구하기 페이지로 리다이렉트");
-            redirectView.setUrl(kakaoRedirectUriSignup);
+            successResponse = BaseResponseTemplate.success(true);
         }
         else {
             log.info("[login 카카오에 토큰 요청 API] 클라이언트를 메인 페이지로 리다이렉트");
-            redirectView.setUrl(kakaoRedirectUriHome);
+            successResponse = BaseResponseTemplate.success(false);
         }
 
-        return redirectView;
+        return ResponseEntity.ok().body(successResponse);
     }
 
     @GetMapping("/consent-starcast")
