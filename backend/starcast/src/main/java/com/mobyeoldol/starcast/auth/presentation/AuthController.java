0c1d087490a2fdd704fb826a48d4758bd2361338
강다영 <gadin8631@naver.com>
2024-10-10 17:13:53 +0900

    fix: 응답 형식 컨벤션 맞추기

--- backend/starcast/src/main/java/com/mobyeoldol/starcast/auth/presentation/AuthController.java
+++ backend/starcast/src/main/java/com/mobyeoldol/starcast/auth/presentation/AuthController.java
@@ -8,9 +8,7 @@ import com.mobyeoldol.starcast.auth.domain.RefreshToken;
 import com.mobyeoldol.starcast.auth.domain.repository.AuthRepository;
 import com.mobyeoldol.starcast.auth.domain.repository.RefreshTokenRepository;
 import com.mobyeoldol.starcast.auth.presentation.request.UpdateUserInfoTmpRequest;
-import com.mobyeoldol.starcast.auth.presentation.response.LogoutResponse;
-import com.mobyeoldol.starcast.auth.presentation.response.UnlinkResponse;
-import com.mobyeoldol.starcast.auth.presentation.response.UpdateUserInfoTmpResponse;
+import com.mobyeoldol.starcast.auth.presentation.response.*;
 import com.mobyeoldol.starcast.global.template.BaseResponseTemplate;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -36,10 +34,10 @@ public class AuthController {
     @GetMapping("/login")
     public ResponseEntity<BaseResponseTemplate<?>> getAccessCode() {
         log.info("[login 유효 인가 코드 요청 API] GET /api/v1/auth/login");
-        String url = authService.getAccessCode();
+        UrlResponse response = new UrlResponse(authService.getAccessCode());
 
         log.info("[login 유효 인가 코드 요청 API] 인가 코드 받아서 프론트로 리턴");
-        BaseResponseTemplate<String> successResponse = BaseResponseTemplate.success(url);
+        BaseResponseTemplate<UrlResponse> successResponse = BaseResponseTemplate.success(response);
         return ResponseEntity.ok().body(successResponse);
     }
 
@@ -75,18 +73,20 @@ public class AuthController {
         log.info("[login 카카오에 토큰 요청 API] kakao id로 auth 엔티티 찾기");
         Optional<Auth> optionalAuth = authRepository.findByKakaoUid(String.valueOf(userInfo.getId()));
 
-        BaseResponseTemplate<Boolean> successResponse;
-
+        BaseResponseTemplate<LoginResponse> successResponse;
+        LoginResponse response;
         if(optionalAuth.isEmpty()) {
             log.info("[login 카카오에 토큰 요청 API] auth 엔티티 존재하지 않음. 따라서 auth 엔티티, userInfoTmp 엔티티 생성");
             authService.generateAuthAndUserInfoTmp(userInfo);
 
             log.info("[login 카카오에 토큰 요청 API] 클라이언트를 동의 구하기 페이지로 리다이렉트");
-            successResponse = BaseResponseTemplate.success(true);
+            response = new LoginResponse(true);
+            successResponse = BaseResponseTemplate.success(response);
         }
         else {
             log.info("[login 카카오에 토큰 요청 API] 클라이언트를 메인 페이지로 리다이렉트");
-            successResponse = BaseResponseTemplate.success(false);
+            response = new LoginResponse(false);
+            successResponse = BaseResponseTemplate.success(response);
         }
 
         return ResponseEntity.ok().body(successResponse);
