699cd614ee2175bdcc9b5db6e4da0ab4f3e5316f
박세은 <seeunbag30@gmail.com>
2024-10-10 17:09:57 +0900

    #55 feat: 주소검색 api 연결

--- frontend/src/components/common/NoContent.tsx
+++ frontend/src/components/common/NoContent.tsx
@@ -6,7 +6,7 @@ type NoContentProps = {
 
 export default function NoContent({ label }: NoContentProps) {
   return (
-    <div className='flex flex-col items-center w-full h-full pt-64'>
+    <div className='flex flex-col items-center w-full h-full gap-2 pt-64'>
       <SvgNoContentIcon className='w-28 h-28' />
       <p className='text-2xs text-bg-50'>{label ?? '내용이 없습니다.'}</p>
     </div>
